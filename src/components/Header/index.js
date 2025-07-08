39e5fa3e4c847a8f32b2dba3bc2ba710fd621b69
Ariel <arielnieto1@hotmail.com>
2025-07-08 17:21:13 -0300

    menu hamburguesa

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,68 @@
 // import { useState } from "react";
-import "./Header.css";
+// import "./Header.css";
+// import NavButton from "./NavButton";
+
+// function Header({ accesos, setActivo, activo }) {
+//   return (
+//     <header className="App-header">
+//       <img src="/img/logoElpidio.png" className="App-logo"  alt="Logo"/>
+//       <ul className="nav">
+//         {accesos.map((acceso) => (
+//             <NavButton
+//               key={acceso.id}
+//               texto={acceso.nombre}
+//               activo={activo === acceso.id}
+//               onClick={() => setActivo(acceso.id)}
+//               />
+//         ))}
+//       </ul>
+//     </header>
+//   );
+// }
+
+
+// export default Header;
+
+import React, { useState } from "react";
 import NavButton from "./NavButton";
+import "./Header.css"; // Importa el CSS
 
 function Header({ accesos, setActivo, activo }) {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  const toggleMenu = () => setMenuAbierto((prev) => !prev);
+
   return (
-    <header className="App-header">
+    <header className="header">
       <img src="/img/logoElpidio.png" className="App-logo"  alt="Logo"/>
-      <ul className="nav">
+      <button
+        className={`hamburguesa ${menuAbierto ? "abierto" : ""}`}
+        onClick={toggleMenu}
+        aria-label="Abrir menú"
+      >
+        <span />
+        <span />
+        <span />
+      </button>
+      <nav className={`menu ${menuAbierto ? "visible" : ""}`}>
         {accesos.map((acceso) => (
-            <NavButton
-              key={acceso.id}
-              texto={acceso.nombre}
-              activo={activo === acceso.id}
-              onClick={() => setActivo(acceso.id)}
-              />
+          <NavButton
+            key={acceso.id}
+            texto={acceso.nombre}
+            activo={activo === acceso.id}
+            onClick={() => {
+              setActivo(acceso.id);
+              setMenuAbierto(false); // Cierra el menú al hacer click
+            }}
+          />
         ))}
-      </ul>
+      </nav>
     </header>
   );
 }
+
+export default Header;
+
 
 
 
@@ -33,4 +77,4 @@ function Header({ accesos, setActivo, activo }) {
 //   </header>
 // }
 
-export default Header
+// export default Header
