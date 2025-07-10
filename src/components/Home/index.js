4567adf24345735e73cea55fda950a12e293b568
Ariel <arielnieto1@hotmail.com>
2025-07-10 18:15:31 -0300

    ajuste header y add mapa

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -32,6 +32,19 @@ function Home(){
         </div>
         
     </section>
+    <section className="maps">
+        <div className="mapsDire">
+            <h2>Av. Juan B. Justo N°7100</h2>
+            <h2>Córdoba</h2>
+        </div>
+        <iframe title="ubicación en Google maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3407.8494428424715!2d-64.1746938!3d-31.335530799999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432990fa9749523%3A0x1895e71c8a50b394!2sHospital%20del%20Noreste%20Elpidio%20Torres!5e0!3m2!1ses!2sar!4v1752173984039!5m2!1ses!2sar" 
+        style={{
+            width: "660",
+            height: "495",
+            border: "0"
+        }}
+         allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </section>
     </div>
 }
 export default Home
